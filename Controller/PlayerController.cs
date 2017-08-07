0fe7cc26fa00cf98a9351a5749f60d310ce12545
Pontus Stjernström <pontus@stjernstrom.nu>
2017-08-07 18:36:23 +0200

    Added controls for second player.

--- Controller/PlayerController.cs
+++ Controller/PlayerController.cs
@@ -21,10 +21,11 @@ namespace DroneWars.Controller
 
         public void Update()
         {
-            CheckKeyboard();
+            CheckKeyboardPlayer1();
+            CheckKeyboardPlayer2();
         }
 
-        private void CheckKeyboard()
+        private void CheckKeyboardPlayer1()
         {
             KeyboardState newState = Keyboard.GetState();
 
@@ -32,30 +33,53 @@ namespace DroneWars.Controller
             {
                 world.Drones[0].Up();
             }
-
-            if (newState.IsKeyDown(Keys.Down))
+            else if (newState.IsKeyDown(Keys.Down))
             {
                 world.Drones[0].Down();
             }
-
-            if (newState.IsKeyDown(Keys.Right))
+            else if (newState.IsKeyDown(Keys.Right))
             {
                 world.Drones[0].Right();
             }
-
-            if (newState.IsKeyDown(Keys.Left))
+            else if (newState.IsKeyDown(Keys.Left))
             {
                 world.Drones[0].Left();
             }
-
-            if (newState.IsKeyDown(Keys.Space))
+            else if (newState.IsKeyDown(Keys.Enter))
             {
-                if (!oldState.IsKeyDown(Keys.Space))
+                if (!oldState.IsKeyDown(Keys.Enter))
                     world.Reset();
             }
-
-            if (newState.GetPressedKeys().Length == 0 && oldState.GetPressedKeys().Length > 0)
+            else
                 world.Drones[0].Release();
+
+            oldState = newState;
+        }
+
+        private void CheckKeyboardPlayer2()
+        {
+            KeyboardState newState = Keyboard.GetState();
+
+            if (newState.IsKeyDown(Keys.W))
+            {
+                world.Drones[1].Up();
+            }
+            else if (newState.IsKeyDown(Keys.S))
+            {
+                world.Drones[1].Down();
+            }
+            else if (newState.IsKeyDown(Keys.D))
+            {
+                world.Drones[1].Right();
+            }
+            else if (newState.IsKeyDown(Keys.A))
+            {
+                world.Drones[1].Left();
+            }
+            else
+            {
+                world.Drones[1].Release();
+            }
 
             oldState = newState;
         }
